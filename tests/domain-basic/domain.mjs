52523b9e70af35f11f11f80209446df1e421a908
Zetsuboii <hmzkrb@gmail.com>
2022-01-04 02:48:30 +0300

    simplify test code

--- tests/domain-basic/domain.mjs
+++ tests/domain-basic/domain.mjs
@@ -3,26 +3,69 @@ import * as domain from './build/domain.main.mjs';
 import {loadStdlib} from '@reach-sh/stdlib';
 
 const sleep = async (t) => await new Promise(r => setTimeout(r, t * 1000));
-const call = async(fn, ...args) => await new Promise(async (res, rej) => {
-  try {
-    const result = await fn(args);
-    if(result[0] === "Some")
-      res(result[1]);
-    else
-      res(null)
+const toDays = (t) => t * 24 * 60 * 60;
+
+const waitFor = async (condGet) => {
+  return await new Promise(async (res) => {
+    while(true) {
+      if(condGet()) {
+        break;
+      }
+      await sleep(2);
+    }
+    res();
+  });
+}
+
+const tryCall = async (fn, ...args) => {
+  return await new Promise(async (res) => {
+    let result = null;
+    while(true) {
+      try {
+        const req = await fn(...args);
+        result = req[1];
+        break;
+      } 
+    catch(e) {}
+    await sleep(2);
   }
-  catch(e) { 
-    console.log("Call failed");
-    res(null) 
-  }
-});
+  res(result);
+})};
+
+const view = async (f, ...args) => {
+  const result = await f(...args)
+  return result[0] === "Some" ? result[1] : null;
+} 
+
+const fmtDetails = async (views) => {
+  const name = await view(views.name);
+  const owner =  await view(views.owner);
+	const resolver =  await view(views.resolver);
+	const ttl =  await view(views.ttl);
+	const price =  await view(views.price);
+	const isAvailable = await view(views.isAvailable);
+
+  const ttld = new Date(ttl * 1000);
+
+  return `Details:\n\tDomain name: ${name}\n\t` +
+  `Owner: ${owner}\n\t` +
+  `Resolves to: ${resolver}\n\t` +
+  `Time to live: ${ttld.toLocaleString()}\n\t` +
+  `Price: ${JSON.stringify(price)}\n\t` +
+  `Is available: ${isAvailable}`
+}
+
+const listDomains = (li) => {
+  li.forEach((v, i) => {
+    console.log(`[${i}]:`, v.appId, "->", v.iface);
+  });
+}
+
 
 (async () => {
   console.log("Loading stdlib...");
   const stdlib = loadStdlib("ETH");
   const startingBalance = stdlib.parseCurrency(100);
-
-  console.log('Launching...');
   await new Promise(res => setTimeout(res, 100));
 
   const [ accConstructor, accCreator, accAlice, accBob ] =
@@ -45,39 +88,21 @@ const call = async(fn, ...args) => await new Promise(async (res, rej) => {
   };
 
   const allowCreator = async () => {
-    while(true) {
-      try {
-        const result = await ctcConstructor.apis.Allower.allow(
-          accCreator.networkAccount.address);
-        console.log("Allowed", result);
-        allowedCreator = true;
-        break;
-      }   
-      catch(e) {} 
-      await sleep(2);
-    }
+    const fns = ctcConstructor.apis.Allower;
+
+    await tryCall(fns.allow, accCreator.networkAccount.address);
+    allowedCreator = true;
   }
 
   const ctcCreator = accCreator.contract(domain);
-  const runCreator = async () => {
-    await new Promise(async (res) => {
-      while(true) {
-        console.log("Creator is waiting for approval");
-        if(allowedCreator) {
-          console.log("Creator is approved");
-          await sleep(1);
-          break;
-        }
-        await sleep(2);
-      }
-      res();
-    });
+  const createDomain = async (domainName) => {
+    await waitFor(() => allowedCreator);
 
     await domain.Creator(ctcCreator, {
       getParams: async () => {
         console.log("Creator is setting domain parameters");
         return {
-          name: "hamza.algo",
+          name: domainName,
           pricePerDay: stdlib.parseCurrency(0.1)
         }
       },
@@ -85,75 +110,51 @@ const call = async(fn, ...args) => await new Promise(async (res, rej) => {
         console.log("Creator announces the contract");
         const address = await ctcCreator.getInfo();
         console.log("Domain contract:", address);
-        const announcerContract = accCreator.contract(announcer, await ctcConstructor.getInfo());
+        const announcerContract = accCreator.contract(announcer, 
+          await ctcConstructor.getInfo());
+        
         const fns = announcerContract.apis.Manager;
-
-        while(true) {
-          try {
-            const result = await fns.register(
-              address,
-              "Domain"
-            );
-            console.log("Registered:", result);
-            break;
-          }
-          catch(e) {
-            console.log("Error with Manager API", e);
-          }
-          await sleep(2);
-        }
+        await tryCall(fns.register, address, "Domain");
       },
     });
   }
 
   const runAlice = async () => {
-    await new Promise(async (res) => {
-      while (true) {
-        console.log("Alice is looking for domains to buy");
-        if(domainsList.length > 0)
-          break;
-        await sleep(2);
-      }
-      res();
-    });
-    console.log(`Alice found an application with ID: ${stdlib.formatAddress(domainsList[0].appId)}`);
+    await waitFor(() => domainsList.length > 0);
+    console.log(`Alice found an application with ID: ${domainsList[0].appId}`);
+
     const domainContract = accAlice.contract(domain, domainsList[0].appId);
+    listDomains(domainsList);
 
-    const domainView = await domainContract.v.name();
-    const domainName = domainView[0] === "Some" ? `${domainView[1]}` : undefined;
-    console.log("Alice learns domain name of the app is", domainName);
-
-    console.log({
+    console.log("Alice learns domain name of the app is", 
+                 await view(domainContract.v.name));
+    
+    console.log(JSON.stringify({
       aliceAddress: accAlice.networkAccount.address,
+      bobAddress: accBob.networkAccount.address,
       contractAddress: domainsList[0].appId,
       creatorAddress: accCreator.networkAccount.address
-    });
-
+    }, null, 2));
+      
     const fns = domainContract.apis.User;
-    await new Promise(async (res) => {
-      while(true) {
-        try {
-          const req = await fns.register(90 * 24 * 60 * 60);
-          // const req = await fns.isAvailable();
-          console.log("Request", req);
-          break;
-        } 
-        catch(e) { }
-        await sleep(2);
-      }
-      res();
-    });
+    console.log("Alice tries to register");
+    await tryCall(fns.register, toDays(90));
+    console.log(await fmtDetails(domainContract.v));
     
-    console.log("New owner: ", await domainContract.v.owner());
-    console.log("New resolver: ", await domainContract.v.resolver());
     
+    console.log("Alice tries to change the resolver");
+    await tryCall(fns.setResolver, accBob.networkAccount.address);
+    console.log(await fmtDetails(domainContract.v));
+
+    process.exit(0);
+
   }
     
   console.log('Starting backends...');
   await Promise.all([
     runConstructor(),
     allowCreator(),
-    runCreator(),
+    createDomain("hamza.algo"),
     runAlice()
   ]);
 })();
