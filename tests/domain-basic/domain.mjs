78815a5f9945889ef5aaf38c63e14790ce6dca3a
Zetsuboii <hmzkrb@gmail.com>
2022-01-04 03:15:53 +0300

    change formatting

--- tests/domain-basic/domain.mjs
+++ tests/domain-basic/domain.mjs
@@ -2,8 +2,14 @@ import * as announcer from './build/announcer.main.mjs';
 import * as domain from './build/domain.main.mjs';
 import {loadStdlib} from '@reach-sh/stdlib';
 
+const NETWORK = process.env["REACH_CONNECTOR_MODE"];
+const stdlib = loadStdlib(NETWORK);
+
 const sleep = async (t) => await new Promise(r => setTimeout(r, t * 1000));
 const toDays = (t) => t * 24 * 60 * 60;
+const getAddress = acc => NETWORK.includes("ALGO") 
+  ? acc.networkAccount.addr
+  : acc.networkAccount.address;
 
 const waitFor = async (condGet) => {
   return await new Promise(async (res) => {
@@ -18,7 +24,8 @@ const waitFor = async (condGet) => {
 }
 
 const tryCall = async (fn, ...args) => {
-  return await new Promise(async (res) => {
+  return await new Promise(async (res, reject) => {
+    let tries = 0;
     let result = null;
     while(true) {
       try {
@@ -26,7 +33,11 @@ const tryCall = async (fn, ...args) => {
         result = req[1];
         break;
       } 
-    catch(e) {}
+    catch(e) { 
+      tries += 1; 
+      if(tries >= 5) 
+        reject("Failed to call API")
+    }
     await sleep(2);
   }
   res(result);
@@ -46,12 +57,15 @@ const fmtDetails = async (views) => {
 	const isAvailable = await view(views.isAvailable);
 
   const ttld = new Date(ttl * 1000);
+  const priceTag = price[0] === "NotForSale"
+    ? "Not For Sale"
+    : "On Sale for " + stdlib.formatCurrency(price[1]) + " network tokens";
 
   return `Details:\n\tDomain name: ${name}\n\t` +
   `Owner: ${owner}\n\t` +
   `Resolves to: ${resolver}\n\t` +
   `Time to live: ${ttld.toLocaleString()}\n\t` +
-  `Price: ${JSON.stringify(price)}\n\t` +
+  `Price: ${priceTag}\n\t` +
   `Is available: ${isAvailable}`
 }
 
@@ -61,10 +75,8 @@ const listDomains = (li) => {
   });
 }
 
-
 (async () => {
   console.log("Loading stdlib...");
-  const stdlib = loadStdlib("ETH");
   const startingBalance = stdlib.parseCurrency(100);
   await new Promise(res => setTimeout(res, 100));
 
@@ -78,7 +90,8 @@ const listDomains = (li) => {
   const runConstructor = async () => {
     await announcer.Constructor(ctcConstructor, {
       printInfo: async () => {
-        console.log("Announcer Contract Info:", await ctcConstructor.getInfo());
+        console.log("Announcer Contract Info:", 
+          JSON.stringify(await ctcConstructor.getInfo()));
       },
       save: (appId, iface) => {
         console.log(`Saved contract info: { appId: ${appId.toString()}, interface: ${iface} }`);
@@ -90,8 +103,9 @@ const listDomains = (li) => {
   const allowCreator = async () => {
     const fns = ctcConstructor.apis.Allower;
 
-    await tryCall(fns.allow, accCreator.networkAccount.address);
+    await tryCall(fns.allow, getAddress(accCreator));
     allowedCreator = true;
+    console.log("Allowed creator");
   }
 
   const ctcCreator = accCreator.contract(domain);
@@ -119,35 +133,48 @@ const listDomains = (li) => {
     });
   }
 
-  const runAlice = async () => {
+  const runUsers = async () => {
     await waitFor(() => domainsList.length > 0);
     console.log(`Alice found an application with ID: ${domainsList[0].appId}`);
 
-    const domainContract = accAlice.contract(domain, domainsList[0].appId);
+    const aliceDomainCtc = accAlice.contract(domain, domainsList[0].appId);
+    const bobDomainCtc = accBob.contract(domain, domainsList[0].appId);
     listDomains(domainsList);
 
     console.log("Alice learns domain name of the app is", 
-                 await view(domainContract.v.name));
+                 await view(aliceDomainCtc.v.name));
     
     console.log(JSON.stringify({
-      aliceAddress: accAlice.networkAccount.address,
-      bobAddress: accBob.networkAccount.address,
+      aliceAddress: getAddress(accAlice),
+      bobAddress: getAddress(accBob),
       contractAddress: domainsList[0].appId,
-      creatorAddress: accCreator.networkAccount.address
+      creatorAddress: getAddress(accCreator)
     }, null, 2));
       
-    const fns = domainContract.apis.User;
+    const alicefns = aliceDomainCtc.apis.User;
+    const bobfns = bobDomainCtc.apis.User;
+
     console.log("Alice tries to register");
-    await tryCall(fns.register, toDays(90));
-    console.log(await fmtDetails(domainContract.v));
-    
+    await tryCall(alicefns.register, toDays(90));
+    console.log(await fmtDetails(aliceDomainCtc.v));
     
     console.log("Alice tries to change the resolver");
-    await tryCall(fns.setResolver, accBob.networkAccount.address);
-    console.log(await fmtDetails(domainContract.v));
+    await tryCall(alicefns.setResolver, getAddress(accBob));
+    console.log(await fmtDetails(aliceDomainCtc.v));
+
+    console.log("Alice tries to transfer it to Bob");
+    await tryCall(alicefns.transferTo, getAddress(accBob));
+    console.log(await fmtDetails(aliceDomainCtc.v));
+
+    console.log("Bob lists the domain for 5 network tokens");
+    await tryCall(bobfns.list, stdlib.parseCurrency(5));
+    console.log(await fmtDetails(aliceDomainCtc.v));
+
+    console.log("Alice buys the domain");
+    await tryCall(alicefns.buy);
+    console.log(await fmtDetails(aliceDomainCtc.v));
 
     process.exit(0);
-
   }
     
   console.log('Starting backends...');
@@ -155,6 +182,6 @@ const listDomains = (li) => {
     runConstructor(),
     allowCreator(),
     createDomain("hamza.algo"),
-    runAlice()
+    runUsers()
   ]);
 })();
